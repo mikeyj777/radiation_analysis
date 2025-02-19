f5521265ce982fbb3b7fb4c79b1d4fb04d0e02b1
Michael James <mikeyj777@gmail.com>
2025-02-19 13:58:58 -0500

    first pass - pws controller

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Functional from './components/Functional';
-import Func001 from './components/001';
-import Func002 from './components/002';
-import Func003 from './components/003';
-import Func004 from './components/004';
-import Func005 from './components/005';
-import Func006 from './components/006';
-import Func007 from './components/007';
-import Func008 from './components/008';
-import Func009 from './components/009';
-import Func010 from './components/010';
-import Func011 from './components/011';
 import './App.css';
 import './styles/global.css';
 
@@ -22,18 +10,6 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/func" element={<Functional />} />
-          <Route path="/001" element={<Func001 />} />
-          <Route path="/002" element={<Func002 />} />
-          <Route path="/003" element={<Func003 />} />
-          <Route path="/004" element={<Func004 />} />
-          <Route path="/005" element={<Func005 />} />
-          <Route path="/006" element={<Func006 />} />
-          <Route path="/007" element={<Func007 />} />
-          <Route path="/008" element={<Func008 />} />
-          <Route path="/009" element={<Func009 />} />
-          <Route path="/010" element={<Func010 />} />
-          <Route path="/011" element={<Func011 />} />
         </Routes>
       </div>
     </Router>
---
+++ server/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+from controllers.pypws_controller import radiation_analysis
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# cors
+app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {  # This specifically matches your API routes
+        "origins": ["http://localhost:3000", "http://WSSAFER02"],
+        "methods": ["GET", "POST", "OPTIONS"],  # Explicitly allow methods
+        "allow_headers": ["Content-Type"]  # Allow common headers
+    }
+})
+
+# endpoint - need radiation analysis
+@app.route('/api/rad', methods=['POST'])
+def rad_route():
+    return radiation_analysis()
